6593c2b0478da0c0ab9e4dc23cba31b24bbd32fd
vedansh tiwari <vt2855028@gmail.com>
2025-02-15 12:54:14 +0530

    Intial commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import path from "path";
 import { send } from "process";
 import { errorHandler } from "./middleware/errorHandler.js";
 import { taxRouter } from "./Routes/tax.routes.js";
 
-dotenv.config({
-  path: "./.env",
-});
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: 'http://localhost:5173',
     credentials: true,
   })
 );
